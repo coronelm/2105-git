41cf9774e2902e7e7dcd53b8a242dac1b0855072
MallariCJ <24-35059@g.batstate-u.edu.ph>
2025-11-07 12:42:44 +0800

    Group 3 created Table Classes.

--- schema.sql
+++ schema.sql
@@ -4,3 +4,4 @@ CREATE TABLE students ( student_id INT PRIMARY KEY, student_name
 VARCHAR(100), grade_level VARCHAR(10) );
 CREATE TABLE teachers (teacher_id INT PRIMARY KEY, teacher_name 
 VARCHAR(100), subject VARCHAR(50) );
+CREATE TABLE classes ( class_id INT PRIMARY KEY, class_name VARCHAR(50), room_no VARCHAR(10));
