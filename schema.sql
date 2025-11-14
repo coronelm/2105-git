b286f1399cad041b7df65fb63d5ff23a71307c47
jermaineparallon27 <jermaineparallon27@gmail.com>
2025-11-14 11:11:17 +0800

    Group 7 created subjects table

--- schema.sql
+++ schema.sql
@@ -10,3 +10,5 @@ FOREIGN KEY (class_id) REFERENCES classes(class_id));
 CREATE TABLE grades (grade_id INT PRIMARY KEY, student_id INT, subject 
 VARCHAR(50), grade FLOAT, FOREIGN KEY (student_id) REFERENCES 
 students(student_id));
+CREATE TABLE subjects ( subject_id INT PRIMARY KEY, subject_name 
+VARCHAR(50), dept_id INT, FOREIGN KEY (dept_id) REFERENCES departments(dept_id) );
